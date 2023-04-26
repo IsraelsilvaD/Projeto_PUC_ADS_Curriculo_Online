63439c1a98394d531d09ddccd9ca287a0242564b
Sara Pereira <126068378+saragilberto@users.noreply.github.com>
2023-04-26 17:40:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -35,3 +35,18 @@ function PageTransitions(){
 }
 
 PageTransitions();
+
+function custom_template(obj){
+  var data = $(obj.element).data();
+  var text = $(obj.element).text();
+  if(data && data['img_src']){
+      img_src = data['img_src'];
+      template = $("<div><img src=\"" + img_src + "\" style=\"width:100%;height:150px;\"/><p style=\"font-weight: 700;font-size:14pt;text-align:center;\">" + text + "</p></div>");
+      return template;
+  }
+}
+var options = {
+'templateSelection': custom_template,
+'templateResult': custom_template,
+}
+$('#id_select2_example').select2(options);
